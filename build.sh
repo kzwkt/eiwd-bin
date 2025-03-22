d3da09da8647cde60426952c62df457d96df8b54
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-22 19:38:08 +0000

    Initial commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+####
+apt update -y
+apt install -y libx11-dev libxft-dev  libxinerama-dev zip git gcc make  sed 
+git clone --depth=1 https://github.com/kzwkt/instantWM
+cd instantWM
+make
+mkdir -p ./dwmpkg/usr/bin
+mkdir -p ./dwmpkg/usr/share/man/man1/
+make PREFIX=/usr DESTDIR="./dwmpkg" install
+zip -r dwmpkg.zip dwmpkg
