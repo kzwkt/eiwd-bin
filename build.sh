2e43e153426c9c53ee626a2d6e94bb5f5476a88b
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-22 20:11:58 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
 ####
 apt update -y
-apt install -y  gcc make  sed  binutils  wget ca-certificates  xz-utils --no-install-recommends
+apt install -y  gcc g++ make  sed  binutils  wget ca-certificates  xz-utils --no-install-recommends
 
 ver=2.22
 echo $ver > ver
 wget https://github.com/illiliti/eiwd/releases/download/$ver-1/iwd-$ver.tar.xz
 tar xf iwd-$ver.tar.xz
-cd iwd-$ver.tar.xz
-
 cd iwd-$ver
 ./configure --disable-dbus --disable-client --disable-monitor  --disable-systemd-service 
 make
