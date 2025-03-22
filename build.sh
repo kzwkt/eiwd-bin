87cfcc55c0688be74ccc5343bfcda9058bef1c07
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-22 20:03:32 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ apt update -y
 apt install -y  gcc make  sed 
 
 ver=2.22
+echo $ver > ver
 wget https://github.com/illiliti/eiwd/releases/download/$ver-1/iwd-$ver.tar.xz
 tar xf iwd-$ver.tar.xz
 cd iwd-$ver.tar.xz
