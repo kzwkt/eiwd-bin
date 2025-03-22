598248051a7bbd51f2cb322ab1899385d42a9c15
kzwkt <9437935+kzwkt@users.noreply.github.com>
2025-03-22 20:09:28 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ####
 apt update -y
-apt install -y  gcc make  sed  binutils  wget --no-install-recommends
+apt install -y  gcc make  sed  binutils  wget ca-certificates --no-install-recommends
 
 ver=2.22
 echo $ver > ver
